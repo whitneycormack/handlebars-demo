216aa67c821f3448d420bfdb40a10b088cc96ef3
Joe Shepherd <joeshepmedia@gmail.com>
2016-11-15 15:01:37 -0600

    Add handlebars templates for cars and nav

--- js/events.js
+++ js/events.js
@@ -1,14 +1,15 @@
 "use strict";
 
 let cardStyle = require('./cards'),
-    cardWrapper = document.getElementById("inventory-cards"),
-    input = document.getElementById("editInput");
+    cardWrapper = document.getElementById("inventory-cards");
+    
 
 function throwError() {
   window.alert("Please click on a card to begin editing");
 }
 
 function activateEvents() {
+  let input = document.getElementById("editInput");
   cardWrapper.addEventListener("click", cardStyle.highlightCard);
   input.addEventListener("keyup", function() {
     var highlighted = document.getElementsByClassName('is-clicked');
